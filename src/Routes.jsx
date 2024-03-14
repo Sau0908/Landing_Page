76c764a2998aa7f04e9c76be75c558306f974c00
Sau0908 <kashyapsaurabh0908@gmail.com>
2024-03-15 02:03:56 +0530

    delete : pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
+
 import LandingPage from "pages/LandingPage";
 
 const ProjectRoutes = () => {
